6b57b25e6086f196b2008acd7ba3ab4dffe25133
Furkan Çelebi <ffurkancelebi@gmail.com>
2024-12-14 21:01:04 +0300

    save filex

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from tkinter import *
+from tkinter import messagebox
 
 FONT = ("verdana", 15 , "bold")
 window = Tk()
 window.title("Secret Notes")
 window.config(padx=30, pady=30)
 
+def save_and_encrypt_notes():
+    title = title_entry.get()
+    message = input_text.get("1.0" , END)
+    master_secret = master_secret_input.get()
+    if len(title) == 0 or len(message) == 0 or len(master_secret) == 0 :
+        messagebox.showinfo(title="Error!", message="Please enter all info.")
+    else:
+        try:
+            with open("mysecret.txt","a") as data_file:
+                data_file.write(f"\n{title}\n{message}")
+        except FileNotFoundError:
+            with open("mysecret.txt","w") as data_file:
+                data_file.write(f"\n{title}\n{message}")
+        finally:
+            title_entry.delete(0, END)
+            master_secret_input.delete(0, END)
+            input_text.delete("1.0", END)
 #uı
 photo = PhotoImage(file="ımage.png")
 photo_label = Label(image=photo)
@@ -22,10 +40,10 @@ input_info_label.pack()
 input_text = Text(width=30, height=25)
 input_text.pack()
 
-master_secret_label= Entry(width=30)
-master_secret_label.pack()
+master_secret_input= Entry(width=30)
+master_secret_input.pack()
 
-save_button = Button(text="Save & Encrypt")
+save_button = Button(text="Save & Encrypt",command=save_and_encrypt_notes)
 save_button.pack()
 
 decrypt_button = Button(text="Encrypt")
